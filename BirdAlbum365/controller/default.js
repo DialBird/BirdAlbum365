4ffd3b44dcbc499db955d69a9481f65421a902ff
DialBird <rememberthatk@icloud.com>
2016-06-07 10:51:17 +0900

    Begoma

--- BirdAlbum365/controller/default.js
+++ BirdAlbum365/controller/default.js
@@ -20,21 +20,29 @@ router.get('/', function(req, res, next) {
 
 //PCでメインページに入ったとき
 router.get('/mainPageForPC', (req, res)=>{
-    
-    //ランダムにroomIDを作成する
-    const roomID = (()=>{
-        const letters = ['a','b','c','d'];
-        const len = letters.length;
-        let id = '';
-        let i;
-        for (i=0;i<len;i++){
-            id += letters[Math.floor(Math.random()*letters.length)];    //アルファベットを適当に配列
-        }
-        id += Math.ceil(Math.random()*1024);            //1から1024までの数字を適当に追加
-        return id;
-//        return 1;
-    })();
-    res.render('mainPageForPC', {roomID : roomID});
+	const ua = req.headers['user-agent'];
+	//スマホだった場合はPCで入る旨を伝えるページへ飛ばす
+	if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+		res.render('loginError', {});
+	//タブレットでもダメ
+	} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+		res.render('loginError', {});
+	} else {
+		//ランダムにroomIDを作成する
+		const roomID = (()=>{
+			const letters = ['a','b','c','d'];
+			const len = letters.length;
+			let id = '';
+			let i;
+			for (i=0;i<len;i++){
+				id += letters[Math.floor(Math.random()*letters.length)];    //アルファベットを適当に配列
+			}
+			id += Math.ceil(Math.random()*1024);            //1から1024までの数字を適当に追加
+			return id;
+	//        return 1;
+		})();
+		res.render('mainPageForPC', {roomID : roomID});
+	}
 });
 
 //SMでメインページに入ったとき
