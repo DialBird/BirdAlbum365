94b8e5470b989140983fae3213b09df5000cad1b
DialBird <rememberthatk@icloud.com>
2016-06-21 20:56:51 +0900

    コメントアウト部分をさくじょ

--- BirdAlbum365/gulp/tasks/createBirdSpriteImg.js
+++ BirdAlbum365/gulp/tasks/createBirdSpriteImg.js
@@ -1,18 +1,18 @@
 
 //複数の鳥の画像を一枚のスプライト画像にするためのタスク
 
-//const gulp = require('gulp');
-//const spritesmith = require('gulp.spritesmith');
-//
-//const config = require('../config');
-//
-//gulp.task('createBirdSpriteImg', ()=>{
-////    gulp.src(path.join(birdImg_dist,'birdsDistorted/**/*.jpg'))
-//    gulp.src(config.img.bldDir + '/birdsDistorted/**/*.jpg')
-//        .pipe(spritesmith({
-//            imgName: 'sprite.png',
-//            cssName: 'sprite.json',
-//            padding:10
-//        }))
-//        .pipe(config.img.bldDir);
-//});
+const gulp = require('gulp');
+const spritesmith = require('gulp.spritesmith');
+
+const config = require('../config');
+
+gulp.task('createBirdSpriteImg', ()=>{
+//    gulp.src(path.join(birdImg_dist,'birdsDistorted/**/*.jpg'))
+   gulp.src(config.img.bldDir + '/birdsDistorted/**/*.jpg')
+       .pipe(spritesmith({
+           imgName: 'sprite.png',
+           cssName: 'sprite.json',
+           padding:10
+       }))
+       .pipe(config.img.bldDir);
+});
